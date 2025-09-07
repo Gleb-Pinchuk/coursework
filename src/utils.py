ffa70aa2da0b4766c098493cc862f7ed3cc5d867
Pinchuk-Gleb <ghliebp@bk.ru>
2025-09-07 12:02:57 +0300

    fix

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,24 @@
-import pandas as pd
 from pathlib import Path
+from typing import Optional
+
 import logging
+import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 DATA_PATH = BASE_DIR / "data" / "operations.xlsx"
 
+logger = logging.getLogger(__name__)
 
-def load_operations(file_path: Path | None = None) -> pd.DataFrame:
+
+def load_operations(file_path: Optional[Path] = None) -> pd.DataFrame:
     """
     Загружает Excel-файл с операциями.
-
     """
     try:
         path = file_path or DATA_PATH
-        logging.info(f"Загрузка операций из файла: {path}")
-        df = pd.read_excel(path, parse_dates=["Дата операции"])
+        logger.info("Загрузка операций из файла: %s", path)
+        df: pd.DataFrame = pd.read_excel(path, parse_dates=["Дата операции"])
         return df
     except Exception as e:
-        logging.error(f"Ошибка при загрузке Excel: {e}", exc_info=True)
+        logger.error("Ошибка при загрузке Excel: %s", e, exc_info=True)
         raise
